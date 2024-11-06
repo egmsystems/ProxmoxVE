635cf27b9ba61a9dd487493ac7b20c6562b56cdc
Edgar Gonzalez <GitHub@EGMSystems.net>
2024-11-06 10:17:36 -0500

    Update nginxproxymanager-install.sh
    Signed-off-by: Edgar Gonzalez <GitHub@EGMSystems.net>

--- install/nginxproxymanager-install.sh
+++ install/nginxproxymanager-install.sh
@@ -155,12 +155,12 @@ DB_MYSQL_HOST="192.168.0.70"
 DB_MYSQL_NAME="nginxProxyManager"
 DB_MYSQL_USER="nginxProxyManager"
 DB_MYSQL_PASSWORD="Gp7mf1MRru3oMGs"
-  cat <<'EOF' >>/root/.bashrc
+  echo "
 export DB_MYSQL_HOST=${DB_MYSQL_HOST}
 export DB_MYSQL_NAME=${DB_MYSQL_NAME}
 export DB_MYSQL_USER=$DB_MYSQL_USER
 export DB_MYSQL_PASSWORD=${DB_MYSQL_PASSWORD}
-EOF
+" >> /root/.bashrc
   cat <<'EOF' >/app/config/production.json
 {
   "database": {
