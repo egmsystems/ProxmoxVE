5af91e672bfc5e6f86837d9cafdfa48831cba8d7
Edgar Gonzalez <GitHub@EGMSystems.net>
2024-11-07 10:48:50 -0500

    Update nginxProxyManager.sh
    Signed-off-by: Edgar Gonzalez <GitHub@EGMSystems.net>

--- lxc/nginxProxyManager.sh
+++ lxc/nginxProxyManager.sh
@@ -8,7 +8,7 @@ MEMORY="1024"
 SWAP="512"
 NET0="name=eth0,bridge=vmbr0,ip=dhcp"
 PASSWORD="mi_contraseña"
-TEMPLATE="local:vztmpl/debian-11-standard_11.0-1_amd64.tar.gz"
+TEMPLATE="local:vztmpl/debian-12-standard_12.7-1_amd64.tar.gz"
 pct create $ID $TEMPLATE --hostname $HOSTNAME --storage $STORAGE --rootfs $ROOTFS --memory $MEMORY --swap $SWAP --net0 $NET0 --password $PASSWORD
 echo "Contenedor creado con ID $ID"
 
