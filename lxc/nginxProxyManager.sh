c0bc6ccdb10611284938cd949e2e0452dc93aa33
Edgar Gonzalez <GitHub@EGMSystems.net>
2024-11-07 09:32:27 -0500

    Create nginxProxyManager.sh
    Signed-off-by: Edgar Gonzalez <GitHub@EGMSystems.net>

---
+++ lxc/nginxProxyManager.sh
@@ -0,0 +1,48 @@
+containerId = 111
+os = "debian"
+version = "12"
+TEMPLATE = local:vztmpl/$os-$version-standard_$version.0-1_amd64.>
+password = prueba12
+pct create $containerId $TEMPLATE \
+--hostname nginxProxyManager \
+--storage local-lvm \
+--rootfs 4 \
+--memory 1024 \
+--swap 512 \
+--net0 name=eth0,bridge=vmbr0,ip=dhcp \
+--password $password
+
+apt -y update
+apt -y install nginx
+
+apt -y install -y nodejs
+git clone https://github.com/jc21/nginx-proxy-manager.git
+cd nginx-proxy-manager
+DB_MYSQL_HOST=192.168.0.70
+DB_MYSQL_NAME=nginxProxyManager
+DB_MYSQL_USER=nginxProxyManager
+DB_MYSQL_PASSWORD=Gp7mf1MRru3oMGs
+  echo "
+export DB_MYSQL_HOST=$DB_MYSQL_HOST
+export DB_MYSQL_NAME="$DB_MYSQL_NAME"
+export DB_MYSQL_USER="$DB_MYSQL_USER"
+export DB_MYSQL_PASSWORD="$DB_MYSQL_PASSWORD"
+" > /root/.bashrc
+  echo "
+{
+
+  \"database\": {
+    \"engine\": \"mysql\",
+    \"host\": \"${DB_MYSQL_HOST}\",
+    \"name\": \"${DB_MYSQL_NAME}\",
+    \"user\": \"${DB_MYSQL_USER}\",
+    \"password\": \"${DB_MYSQL_PASSWORD}\",
+    \"port\": 3306
+  }
+}" > config/production.json
+npm install
+npm run build
+npm start
+
+cd ..
+#rm -r nginx-proxy-manager
